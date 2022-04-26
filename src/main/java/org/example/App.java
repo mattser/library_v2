45e300b2e091a885ff46d6e9da64488644be246b
mattser <jomatser@gmail.com>
2022-04-26 08:48:50 +0100

    started building library class

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -12,17 +12,12 @@ public class App
     public static void main( String[] args ) {
         BookRepository bookRepo = new BookRepository();
 
-        bookRepo.printBooks();
-
         UserRepository userRepo = new UserRepository();
-
-        userRepo.printUsers();
 
         LoanRepository loanRepo = new LoanRepository();
 
-        loanRepo.printLoans();
+        Library library = new Library(bookRepo,loanRepo,userRepo);
 
-
-
+        library.start();
     }
 }
--- src/main/java/org/example/Library.java
+++ src/main/java/org/example/Library.java
@@ -1,6 +1,32 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Library {
 
+    private final BookRepository bookRepository;
+    private final LoanRepository loanRepository;
+    private final UserRepository userRepository;
+
+    public Library() {
+        this.bookRepository = new BookRepository();
+        this.loanRepository = new LoanRepository();
+        this.userRepository = new UserRepository();
+    }
+
+    public Library(BookRepository bookRepository, LoanRepository loanRepository, UserRepository userRepository) {
+        this.bookRepository = bookRepository;
+        this.loanRepository = loanRepository;
+        this.userRepository = userRepository;
+    }
+
+    public void start() {
+
+    }
+
+    private void authenticate() {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Please Type your User Name");
+    }
 
 }
